c41df5698561e454595a78e677f3f0ab634736a7
William B. Wettersten <wbw20@case.edu>
2013-09-09 13:46:59 -0400

    entities

--- src/com/casehacks/swipe_android/model/Buy.java
+++ src/com/casehacks/swipe_android/model/Buy.java
@@ -2,4 +2,19 @@ package com.casehacks.swipe_android.model;
 
 public class Buy {
 
+    private String name;
+    private Double price;
+
+    public Buy(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Double price() {
+        return price;
+    }
 }
--- src/com/casehacks/swipe_android/model/Sell.java
+++ src/com/casehacks/swipe_android/model/Sell.java
@@ -2,4 +2,19 @@ package com.casehacks.swipe_android.model;
 
 public class Sell {
 
+    private String name;
+    private Double price;
+
+    public Sell(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Double price() {
+        return price;
+    }
 }
