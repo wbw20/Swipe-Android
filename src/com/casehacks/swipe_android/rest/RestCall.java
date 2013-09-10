4c0a3556202296a4b9861987e0ea7208856a1367
William B. Wettersten <wbw20@case.edu>
2013-09-10 09:12:57 -0400

    Rest utilities.  Also checking in a jar :(

--- src/com/casehacks/swipe_android/rest/RestCall.java
+++ src/com/casehacks/swipe_android/rest/RestCall.java
@@ -1,5 +1,51 @@
 package com.casehacks.swipe_android.rest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class RestCall {
 
+    private final static String host = "http://209.20.92.28:3000/";
+    private static Gson gson = new Gson();
+
+    /**
+     * Get a json array and reflectively cast it.
+     * 
+     *       List<Buy> buys = get("buy", Buy.class);
+     * 
+     * The above example gets a list of all spots.
+     * 
+     * @param path The url of the server resource.
+     * @param type The class to cast the response to.
+     * 
+     * @return A list of the requested server entities.
+     */
+    public static <T> T get(String path, Class<T> clazz) {
+        T results = null;
+        TypeToken<ArrayList<T>> type = new TypeToken<ArrayList<T>>(){};
+
+        try {
+            URL url = new URL(host + path);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            InputStream in = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            results = gson.fromJson(reader.readLine(), type.getType());
+
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace(); //swallow
+        }
+
+        return results;
+    }
 }
