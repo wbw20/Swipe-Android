3183574584e3353e12735b86c826270e10f1f167
William B. Wettersten <wbw20@case.edu>
2013-09-14 11:33:33 -0400

    stub out views for buy and sell

---
+++ src/com/casehacks/swipe_android/views/Buy.java
@@ -0,0 +1,15 @@
+package com.casehacks.swipe_android.views;
+
+import com.casehacks.swipe_android.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Buy extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buy);
+    }
+}
