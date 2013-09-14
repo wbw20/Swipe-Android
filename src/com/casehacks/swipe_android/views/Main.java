f8f2e652d4451480e761fabc405ec791d3e756d4
William B. Wettersten <wbw20@case.edu>
2013-09-14 12:17:36 -0400

    click listeners

--- src/com/casehacks/swipe_android/views/Main.java
+++ src/com/casehacks/swipe_android/views/Main.java
@@ -3,7 +3,10 @@ package com.casehacks.swipe_android.views;
 import com.casehacks.swipe_android.R;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.app.Activity;
+import android.content.Intent;
 
 public class Main extends Activity {
 
@@ -11,5 +14,21 @@ public class Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        findViewById(R.id.buy).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Buy.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.sell).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Sell.class);
+                startActivity(intent);
+            }
+        });
     }
 }
