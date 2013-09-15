bc8939c3c26074fae25bc015eab97f6037c6abc9
William B. Wettersten <wbw20@case.edu>
2013-09-15 18:36:42 -0400

    populate listview

--- src/com/casehacks/swipe_android/views/Buy.java
+++ src/com/casehacks/swipe_android/views/Buy.java
@@ -1,9 +1,11 @@
 package com.casehacks.swipe_android.views;
 
 import com.casehacks.swipe_android.R;
+import com.casehacks.swipe_android.util.buy.BuyHelper;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class Buy extends Activity {
 
@@ -11,5 +13,7 @@ public class Buy extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buy);
+
+        BuyHelper.populate((ListView)findViewById(R.id.list));
     }
 }
