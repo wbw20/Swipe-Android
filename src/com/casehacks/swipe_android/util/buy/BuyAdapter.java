c3436ba772a51771291084fad96ccde29464a367
William B. Wettersten <wbw20@case.edu>
2013-09-15 19:57:08 -0400

    real data in lists

--- src/com/casehacks/swipe_android/util/buy/BuyAdapter.java
+++ src/com/casehacks/swipe_android/util/buy/BuyAdapter.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.casehacks.swipe_android.R;
 import com.casehacks.swipe_android.model.Buy;
@@ -21,6 +22,8 @@ public class BuyAdapter extends GenericAdapter<Buy> {
     public View getView(int position, View convertView, ViewGroup parent) {
         if (views().get(entities().get(position)) == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.buy_list_item, null);
+            ((TextView)convertView.findViewById(R.id.name)).setText(entities().get(position).name());
+            ((TextView)convertView.findViewById(R.id.price)).setText(entities().get(position).price().toString());
             views().put(entities().get(position), convertView);
         }
 
