d91cd52a9949d0ce9818c17e196f117d39413215
William B. Wettersten <wbw20@case.edu>
2013-09-15 17:33:25 -0400

    This marks the firs time I've been impressed with Java design work

---
+++ src/com/casehacks/swipe_android/util/buy/BuyAdapter.java
@@ -0,0 +1,17 @@
+package com.casehacks.swipe_android.util.buy;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.casehacks.swipe_android.model.Buy;
+import com.casehacks.swipe_android.util.GenericAdapter;
+
+public class BuyAdapter extends GenericAdapter<Buy> {
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
