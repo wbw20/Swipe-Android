78467b8a28d4816ae77e50deefc71135684341d9
William B. Wettersten <wbw20@case.edu>
2013-09-15 17:22:38 -0400

    stub out a buy list

---
+++ src/com/casehacks/swipe_android/util/GenericAdapter.java
@@ -0,0 +1,37 @@
+package com.casehacks.swipe_android.util;
+
+import java.util.Map;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class GenericAdapter<T> extends BaseAdapter {
+
+    Map<T, View> entities;
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
