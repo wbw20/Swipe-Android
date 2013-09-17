c6d73f93fd54ebd09e30fab192839e05d54b178e
William B. Wettersten <wbw20@case.edu>
2013-09-17 09:21:12 -0400

    simple post function

--- src/com/casehacks/swipe_android/rest/RestCall.java
+++ src/com/casehacks/swipe_android/rest/RestCall.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -46,6 +47,38 @@ public class RestCall {
         }
 
         return results;
+    }
+
+    /**
+     * This is a post method for custom urls that allows for posting entities to any url.  Upon
+     * receiving s successful response this method will reconstruct the entity as it now exists on
+     * the server.
+     * 
+     * @param path The url extension to post to.
+     * @param data The entity which will be posted.
+     * @return The entity as it exists after going through the server.
+     */
+    public static Object post(String path, Object data) {
+        Object result = null;
+        try {
+            URL url = new URL(host + path);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+            OutputStream os = connection.getOutputStream();
+            os.write(gson.toJson(data).getBytes());
+            os.flush();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            result = gson.fromJson(reader.readLine(), data.getClass());
+
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace(); //swallow
+        }
+
+        return result;
     }
 
     private static String read(BufferedReader reader) throws IOException {
