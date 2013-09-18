2df2d27606bb2249507aa385e21497b8c907487b
William B. Wettersten <wbw20@case.edu>
2013-09-18 13:35:36 -0400

    wire everything together

--- src/com/casehacks/swipe_android/views/Buy.java
+++ src/com/casehacks/swipe_android/views/Buy.java
@@ -5,6 +5,9 @@ import com.casehacks.swipe_android.util.buy.BuyHelper;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class Buy extends Activity {
@@ -14,6 +17,22 @@ public class Buy extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buy);
 
+        findViewById(R.id.offer).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BuyHelper.post((ListView)findViewById(R.id.list), 
+                    new com.casehacks.swipe_android.model.Buy(text(R.id.name), num(R.id.price)));
+            }
+        });
+
         BuyHelper.populate((ListView)findViewById(R.id.list));
+    }
+
+    private String text(Integer id) {
+        return ((EditText)findViewById(id)).toString();
+    }
+
+    private Double num(Integer id) {
+        return Double.parseDouble(((EditText)findViewById(id)).toString());
     }
 }
