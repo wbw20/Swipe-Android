dd497aa73414c98d91ae38217b6a16dc5ce8e496
William B. Wettersten <wbw20@case.edu>
2013-09-19 15:32:00 -0400

    Sell page works

--- src/com/casehacks/swipe_android/views/Sell.java
+++ src/com/casehacks/swipe_android/views/Sell.java
@@ -1,9 +1,14 @@
 package com.casehacks.swipe_android.views;
 
 import com.casehacks.swipe_android.R;
+import com.casehacks.swipe_android.util.sell.SellHelper;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class Sell extends Activity {
 
@@ -11,5 +16,23 @@ public class Sell extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sell);
+
+        findViewById(R.id.offer).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SellHelper.post((ListView)findViewById(R.id.list), 
+                    new com.casehacks.swipe_android.model.Sell(text(R.id.offer_name), num(R.id.offer_price)));
+            }
+        });
+
+        SellHelper.populate((ListView)findViewById(R.id.list));
+    }
+
+    private String text(Integer id) {
+        return ((EditText)findViewById(id)).getText().toString();
+    }
+
+    private Double num(Integer id) {
+        return Double.parseDouble(((EditText)findViewById(id)).getText().toString());
     }
 }
