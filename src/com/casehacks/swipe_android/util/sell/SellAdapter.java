4ca639d21d6707984dd24f495d4cd1e38b799a10
William B. Wettersten <wbw20@case.edu>
2013-09-19 14:53:42 -0400

    stub out SellAdapter

---
+++ src/com/casehacks/swipe_android/util/sell/SellAdapter.java
@@ -0,0 +1,24 @@
+package com.casehacks.swipe_android.util.sell;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.casehacks.swipe_android.model.Sell;
+import com.casehacks.swipe_android.util.GenericAdapter;
+
+public class SellAdapter extends GenericAdapter<Sell> {
+
+    public SellAdapter(Context context, List<Sell> list) {
+        super(context, list);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
