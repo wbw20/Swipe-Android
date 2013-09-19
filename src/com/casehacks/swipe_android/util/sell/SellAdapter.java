b13f83b34c7202584cbc493f13f9c16e1adf1b13
William B. Wettersten <wbw20@case.edu>
2013-09-19 14:55:01 -0400

    copy pasta

--- src/com/casehacks/swipe_android/util/sell/SellAdapter.java
+++ src/com/casehacks/swipe_android/util/sell/SellAdapter.java
@@ -3,9 +3,12 @@ package com.casehacks.swipe_android.util.sell;
 import java.util.List;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.casehacks.swipe_android.R;
 import com.casehacks.swipe_android.model.Sell;
 import com.casehacks.swipe_android.util.GenericAdapter;
 
@@ -13,12 +16,17 @@ public class SellAdapter extends GenericAdapter<Sell> {
 
     public SellAdapter(Context context, List<Sell> list) {
         super(context, list);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // TODO Auto-generated method stub
-        return null;
+        if (views().get(entities().get(position)) == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.sell_list_item, null);
+            ((TextView)convertView.findViewById(R.id.name)).setText(entities().get(position).name());
+            ((TextView)convertView.findViewById(R.id.price)).setText(entities().get(position).price().toString());
+            views().put(entities().get(position), convertView);
+        }
+
+        return views().get(entities().get(position));
     }
 }
